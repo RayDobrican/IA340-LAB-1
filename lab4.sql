bddfbf2c93adbc6877f283d1dee0e9af8d29eb30
RayDobrican <raresdobrican76@gmail.com>
2024-09-18 17:42:36 -0400

    lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -1,1 +1,5 @@
-
+--Q4.1
+/'We cannot delete VA because it is connected to the fips which is a foreign key. VA records are in name and population therefore we cannot delete it'/
+  
+--Q4.2
+/'We cannot insert the code because there is no valued fips that equals 80'/
